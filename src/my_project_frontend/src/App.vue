98ea0223329a68c862c561a98fff136e188be589
MrocznyKnight Dawid <kaczmarczykd111@gmail.com>
2024-12-04 17:30:25 +0000

    projekt 04.12

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let blogs = ref([]);
 
 async function handleSubmit(e) {
   e.preventDefault();
@@ -9,11 +9,22 @@ async function handleSubmit(e) {
   const title = target.querySelector('#title').value;
   const content = target.querySelector('#content').value;
   const tags = target.querySelector('#tags').value;
-  const splitedTags=tags.split(",")
+  const splitedTags = tags.split(",")
 
-  await my_project_backend.add_blog(title,content,splitedTags)
-  }
+  await my_project_backend.add_blog(title, content, splitedTags)
+  await getBlogs()
+}
 
+async function getBlogs() {
+  const tempBlogs = await my_project_backend.get_blogs()
+  blogs.value = tempBlogs.map((blog) => {
+    return {
+      ...blog,
+      date: blog.date.toString()
+    }
+  })
+}
+getBlogs()
 </script>
 
 <template>
@@ -22,11 +33,11 @@ async function handleSubmit(e) {
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <div><P>Title: </P> <input id="title" alt="title" type="text" /></div>
-      <div><P>Content: </P> <input id="content" alt="content" type="text" /></div>
-      <div><P>Tags: </P> <input id="tags" alt="tags" type="text" /></div>
+      <div><p>Title: </p><input id="title" alt="title" type="text" /></div>
+      <div><p>Content: </p><input id="content" alt="content" type="text" /></div>
+      <div><p>Tags: </p><input id="tags" alt="tags" type="text" /></div>
       <button type="submit">Click to add!</button>
     </form>
-    
+    {{ blogs }}
   </main>
 </template>
